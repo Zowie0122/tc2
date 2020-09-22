eff076faa5af62703b2a01eea601544eea6b03fe
Zowie0122 <minjiayi22@gmail.com>
2020-09-22 22:44:56 +0900

    added build folder

---
+++ build/precache-manifest.4068b604d9509797b1b3e29d71d63a08.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c61b850cada8bbeb3482cbf8d5f8d69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce243f253a93f8b54a4",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "3019752707ae8d954e5d",
+    "url": "/static/css/main.d6207f84.chunk.css"
+  },
+  {
+    "revision": "4ce243f253a93f8b54a4",
+    "url": "/static/js/2.cc43ce7a.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/static/js/2.cc43ce7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3019752707ae8d954e5d",
+    "url": "/static/js/main.9009eb0f.chunk.js"
+  },
+  {
+    "revision": "e04b5e304698ca6b50f5",
+    "url": "/static/js/runtime-main.8f2a1934.js"
+  },
+  {
+    "revision": "8578964030ccc3d9477567e07fbf5b18",
+    "url": "/static/media/A-OTF-GothicMB101Pro-Bold.85789640.otf"
+  },
+  {
+    "revision": "1ddf906cd58ef54139b4ec820d9c2672",
+    "url": "/static/media/A-OTF-GothicMB101Pro-Light.1ddf906c.otf"
+  },
+  {
+    "revision": "52c5c7cbbe8e34733a8a1bd7581dd7bc",
+    "url": "/static/media/A-OTF-GothicMB101Pro-Medium.52c5c7cb.otf"
+  },
+  {
+    "revision": "2df2a04f507be24363a8f88f89f6165d",
+    "url": "/static/media/A-OTF-GothicMB101Pro-Reg.2df2a04f.otf"
+  },
+  {
+    "revision": "904d4d9d096af176560ee153d2a4bd42",
+    "url": "/static/media/DINCondensed-Bold.904d4d9d.ttf"
+  },
+  {
+    "revision": "40acaef1d74bcfe3ee64622be323d2d8",
+    "url": "/static/media/blog1.40acaef1.png"
+  },
+  {
+    "revision": "5035d2b638dc1942a27a543e0c54f93d",
+    "url": "/static/media/blog2.5035d2b6.png"
+  },
+  {
+    "revision": "17ba8c17c3621bf0b2eab6e6882f7eb3",
+    "url": "/static/media/blog3.17ba8c17.png"
+  },
+  {
+    "revision": "bee7ee74820cdd24a9daf497b2631732",
+    "url": "/static/media/mv.bee7ee74.png"
+  }
+]);
